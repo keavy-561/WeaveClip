ff7bdff1a6f4d6597f9dec601a0ce275dd0c7e91
keavy-561 <shenkfan561@gmail.com>
2026-08-17 15:59:17 +0800

    fix: update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import '@douyinfe/semi-ui/dist/css/semi.min.css';
 import '@/styles/themes/dark.scss';
 import '@/styles/themes/light.scss';
 import '@/styles/global.scss';
